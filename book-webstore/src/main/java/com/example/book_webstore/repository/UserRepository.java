35419860658d12d621b7ad4e3387bd5132bc7dd3
danhdaden777 <minhdanh2824@gmail.com>
2026-04-06 16:11:09 +0700

    BWK-29: update order feature for customer

--- book-webstore/src/main/java/com/example/book_webstore/repository/UserRepository.java
+++ book-webstore/src/main/java/com/example/book_webstore/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package com.example.book_webstore.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.book_webstore.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    List<User> findByRoleOrderByNameAsc(User.Role role);
 
 }
